5723b4398d86c93a2f49e67dae1bc2f235707f5c
adairaxe <aabrigo@espol.edu.ec>
2023-06-10 00:13:19 -0500

    Agregación de palabras claves, opreadores y ejemplo. Todo está validado

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ reserved = {
     #Fin aporte Juan Guadalupe
 
     #Inicio aporte Adair Abrigo
-
+    "if": "IF",
+    "while": "WHILE",
+    "public": "PUBLIC",
+    "static": "STATIC",
+    "long": "LONG",
+    "return": "RETURN",
+    "int": "INT",
+    "for": "FOR",
     #Fin aporte Adair Abrigo
     #Inicio aporte David Rivera
 
@@ -32,15 +39,24 @@ tokens = (
     'VARIABLE',
     'LPARENT',
     'RPARENT',
-    'LLLAVE',
-    'RLLAVE',
-    'LCORCHETE',
-    'RCORCHETE',
-    'PUNTO',
+    'LBRACKET',
+    'RBRACKET',
+    'LKEY',
+    'RKEY',
+    'DOT',
     #Fin aporte Juan Guadalupe
 
     #Inicio aporte Adair Abrigo
-
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'GREATER_THAN',
+    'SMALLER_THAN',
+    'EQUAL_COMPARATION',
+    'EQUAL',
+    'DOTANDCOMMA',
     #Fin aporte Adair Abrigo
     #Inicio aporte David Rivera
 
@@ -57,14 +73,23 @@ tokens = (
 #Inicio aporte Juan Guadalupe
 t_LPARENT = r'\('
 t_RPARENT = r'\)'
-t_LLLAVE = r'\['
-t_RLLAVE = r'\]'
-t_LCORCHETE = r'\{'
-t_RCORCHETE = r'\}'
-t_PUNTO   = r'\.'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LKEY = r'\{'
+t_RKEY = r'\}'
+t_DOT   = r'\.'
 #Fin aporte Juan Guadalupe
 #Inicio aporte Adair Abrigo
-
+t_NUMBER = r'\d+'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_GREATER_THAN = r'>'
+t_SMALLER_THAN = r'<'
+t_EQUAL_COMPARATION = r'=='
+t_EQUAL = r'='
+t_DOTANDCOMMA = r';'
 #Fin aporte Adair Abrigo
 #Inicio aporte David Rivera
 
@@ -111,19 +136,27 @@ coment
 multilinea
 */
 public void MetodoBurbuja()
-        {
-            int t;
-            for (int a = 1; a < vector.Length; a++)
-                for (int b = vector.Length - 1; b >= a; b--)
-                {
-                    if (vector[b - 1] > vector[b])
-                    {
-                        t = vector[b - 1];
-                        vector[b - 1] = vector[b];
-                        vector[b] = t;
-                    }
-                }
-        }
+  {
+    int t;
+    for (int a = 1; a < vector.Length; a++)
+      for (int b = vector.Length - 1; b >= a; b--)
+      {
+          if (vector[b - 1] > vector[b])
+          {
+              t = vector[b - 1];
+              vector[b - 1] = vector[b];
+              vector[b] = t;
+          }
+      }
+  }
+
+public static long Factorial(int n) 
+  {
+    if (n==1)               // Aseguramos que termine (caso base)
+        return 1;
+    return n * Factorial(n-1);  // Si no es 1, sigue la recursión
+  }
+
     '''.lower()
  
  #Datos de entrada
