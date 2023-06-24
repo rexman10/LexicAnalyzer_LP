531fb04dfdf5e4c136437242c92bb955ec4ee559
adairaxe <aabrigo@espol.edu.ec>
2023-06-24 13:21:57 -0500

    Ordenamiento de funciones de declaracion, está más estructurado

--- sintactico.py
+++ sintactico.py
@@ -3,23 +3,58 @@ import ply.yacc as yacc
 from main import tokens
 
 """ Ejemplos con los que probar:
-using System public class clase1 { adas } 
-using System public class clase1 { try{ acacsacasc } catch (exception e){ dsadas } }
- """
+using System; public class clase1 { adas } 
+using System; public class clase1 { try{ acacsacasc } catch (exception e){ dsadas } }
+using System; public class clase1 { try{ acacsacasc } catch (exception e){ dsadas } finally { final } }
+using System; public class clase1 { const int var2 = 14 , var3 = 15 ; }
+"""
  
 def p_program(p):
     'program : block_using block_publicClass'
 
 def p_block_using(p):
-    'block_using : USING SYSTEM'
+    'block_using : USING SYSTEM DOTANDCOMMA'
 
 def p_block_publicClass(p):
     'block_publicClass : PUBLIC CLASS VARIABLE LKEY block_code RKEY'
 
-""" def p_block_try_catch(p):
-    '''block_try_catch : try_catch_simply
-                        | try_catch_finaly
-    ''' """
+
+
+def p_block_code(p):
+    '''block_code : def_const_or_var
+                    | block_try_catch
+                    | VARIABLE
+    '''
+
+
+
+
+""" Declaracion de variables y constantes """
+
+def p_def_const_or_var(p):
+    '''def_const_or_var : constant_assignation
+                        | variable_assignation
+    '''
+
+def p_variable_assignation(p):
+    '''variable_assignation : access_modifiers data_type multi_or_one_assignation DOTANDCOMMA
+                                    | data_type multi_or_one_assignation DOTANDCOMMA
+                                    | data_type VARIABLE DOTANDCOMMA
+                                    | multi_or_one_assignation DOTANDCOMMA
+    '''
+def p_constant_assignation(p):
+    '''constant_assignation : access_modifiers CONST data_type multi_or_one_assignation DOTANDCOMMA
+                            | CONST data_type multi_or_one_assignation DOTANDCOMMA
+                            | CONST data_type VARIABLE DOTANDCOMMA
+    '''
+
+def p_multi_or_one_assignation(p):
+    '''multi_or_one_assignation : assignation_key_value
+                                | assignation_key_value COMMA multi_or_one_assignation
+    '''
+
+def p_assignation_key_value(p):
+    'assignation_key_value : VARIABLE ASSIGNATION value'
 
 def p_data_type(p):
     '''data_type    : CHARTYPE
@@ -30,37 +65,15 @@ def p_data_type(p):
                     | BOOLTYPE
     '''
 
-def p_access_modifiers(p):
-    '''access_modifiers : PUBLIC
-                        | PRIVATE
-    '''
 
-def p_arithmetic_operation(p):
-    '''arithmetic_operation : value_numeric
-                            | value_numeric arithmetic_operator arithmetic_operation
-    '''
 
-def p_value_numeric(p):
-    '''value_numeric    : INTEGER
-                        | FLOAT_NUMBER
-                        | DECIMAL_NUMBER
-                        | VARIABLE
-    '''
 
-def p_value_logic(p):
-    '''value_logic  : BOOLTRUE
-                    | BOOLFALSE
-                    | VARIABLE
-    '''
+""" Tipo de valores validos para asignar """
 
-def p_logic_operation(p):
-    '''logic_operation  : value_logic
-                        | value_logic logic_operator logic_operation
-    '''
-
-def p_concatenation(p):
-    '''concatenation    : STRING
-                        | STRING PLUS concatenation
+def p_value(p):
+    '''value    : value_numeric
+                | value_logic
+                | value_string
     '''
 
 def p_value_string(p):
@@ -71,10 +84,55 @@ def p_value_string(p):
                     | concatenation
     '''
 
-def p_value(p):
-    '''value    : value_numeric
-                | value_logic
-                | value_string
+def p_value_logic(p):
+    '''value_logic  : BOOLTRUE
+                    | BOOLFALSE
+    '''
+
+def p_value_numeric(p):
+    '''value_numeric    : INTEGER
+                        | FLOAT_NUMBER
+                        | DECIMAL_NUMBER
+    '''
+
+
+
+
+""" Estructuras de control """
+
+def p_block_try_catch(p):
+    '''block_try_catch : try_catch_simply
+                        | try_catch_finally
+    '''
+
+def p_try_catch_simply(p):
+    'try_catch_simply : TRY LKEY block_code RKEY CATCH LPARENT EXCEPTION VARIABLE RPARENT LKEY block_code RKEY'
+    
+def p_try_catch_finally(p):
+    'try_catch_finally : TRY LKEY block_code RKEY CATCH LPARENT EXCEPTION VARIABLE RPARENT LKEY block_code RKEY FINALLY LKEY block_code RKEY'
+
+
+def p_access_modifiers(p):
+    '''access_modifiers : PUBLIC
+                        | PRIVATE
+    '''
+
+
+
+
+def p_arithmetic_operation(p):
+    '''arithmetic_operation : value_numeric
+                            | value_numeric arithmetic_operator arithmetic_operation
+    '''
+
+def p_logic_operation(p):
+    '''logic_operation  : value_logic
+                        | value_logic logic_operator logic_operation
+    '''
+
+def p_concatenation(p):
+    '''concatenation    : STRING
+                        | STRING PLUS concatenation
     '''
 
 def p_logic_operator(p):
@@ -97,36 +155,10 @@ def p_arithmetic_operator(p):
                             | DIVIDE
                             | PERCENT
     '''
-def p_constant_assignation(p):
-    '''constant_assignation : access_modifiers CONST data_type VARIABLE ASSIGNATION value
-                            | CONST data_type VARIABLE ASSIGNATION value
-    '''
-
-def p_variable_assignation_simple(p):
-    '''variable_assignation_simple  : access_modifiers data_type VARIABLE ASSIGNATION value
-                                    | data_type VARIABLE ASSIGNATION value
-                                    | data_type VARIABLE
-                                    | VARIABLE ASSIGNATION value
-    '''
-
-def p_variable_multivalue(p):
-    '''variable_multivalue  : variable_assignation_simple
-                            | variable_assignation_simple COMMA variable_multivalue
-    '''
 
 def p_variable_assignation_multiline(p):
     '''variable_assignation_multiline   : 
     '''
-
-def p_block_code(p):
-    '''block_code : VARIABLE
-                    | try_catch_simply
-    '''
-
-def p_try_catch_simply(p):
-    'try_catch_simply : TRY LKEY block_code RKEY CATCH LPARENT EXCEPTION VARIABLE RPARENT LKEY block_code RKEY'
-    
-
 
 
 def p_error(p):
